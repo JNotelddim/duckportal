ebdb4d40478ae04252a8bfd87d00405830b81e4a
JNotelddim <jaredmiddleton3.14@gmail.com>
2019-10-27 11:54:22 -0700

    Add basic form controls for inputting data
    needs tweaking and submit handling

--- src/pages/DuckFeeding.js
+++ src/pages/DuckFeeding.js
@@ -1,17 +1,56 @@
 import React, { Component } from 'react';
+import { TextField, FormControl, InputLabel, Input } from '@material-ui/core';
 
 import Layout from '../comp/Layout.js';
-import FormInput from '../comp/FormInput';
 
 export default class DuckFeeding extends Component{
     
+    handleSubmit(params){
+        alert(params);
+    }
+
     render() {
         return <div>
             <Layout>
                 <p>This is the duck-feeding page.</p>
                 
-                <form>
-                    <FormInput label="Location" type="text"></FormInput>
+                <form onsubmit={this.handleSubmit}>
+                    <FormControl>
+                        <InputLabel>Time of Feeding</InputLabel>
+                        <Input type="datetime-local" placeholder=""></Input>
+                    </FormControl>
+                    <br/>
+                    
+                    <FormControl>
+                        <InputLabel>Number of ducks present</InputLabel>
+                        <Input type="number"></Input>
+                    </FormControl>
+                    <br/>
+                    
+
+                    <TextField 
+                        id="standard-location"
+                        label="Location"
+                        />
+                    <br/>
+                    
+                    <TextField 
+                        id="standard-food"
+                        label="Food"
+                        />
+                    <br/>
+
+                    <TextField 
+                        id="standard-food-type"
+                        label="Food Type"
+                        />
+                    <br/>
+
+                    <FormControl>
+                        <InputLabel>Amount of food</InputLabel>
+                        <Input type="text"></Input>
+                    </FormControl>
+                    <br/>
                     
                 </form>
 
